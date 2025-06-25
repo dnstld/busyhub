600c0ccaf5270f8717af0c0e465e3f47c5cd58ab
Denis Toledo <detoledo.denis@gmail.com>
2025-06-25 17:27:03 +0200

    redirect to dashboard after login

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const session = await auth();
+
+  if (!session) return redirect('/');
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
--- src/ui/sign-in.tsx
+++ src/ui/sign-in.tsx
@@ -24,7 +24,9 @@ export default async function SignIn() {
     <form
       action={async () => {
         'use server';
-        await signIn('google');
+        await signIn('google', {
+          redirectTo: '/dashboard',
+        });
       }}
     >
       <button type="submit">Signin with Google</button>
