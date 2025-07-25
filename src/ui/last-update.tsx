68bcf471ecd06c562996f4e5fe18ca9c322c1e21
Denis Toledo <detoledo.denis@gmail.com>
2025-07-25 13:06:07 +0200

    update last update

--- src/ui/last-update.tsx
+++ src/ui/last-update.tsx
@@ -1,5 +1,5 @@
 const LastUpdate = () => {
-  return <p className="text-xs text-gray-400">Last Update: July 22, 2025</p>;
+  return <p className="text-xs text-gray-400">Last Update: July 25, 2025</p>;
 };
 
 export default LastUpdate;
